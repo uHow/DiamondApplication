edb57d59e57ea8998c8fde020defd6d9acde2d6a
uHow <micrycew@student.pg.gda.pl>
2017-05-26 21:13:59 +0200

    Dodano klasę i pakiety umożliwiające połączenie i obsługę bazy danych. Poprawiono konstruktor klasy Diamond.

--- DiamondApplication/Diamond.cs
+++ DiamondApplication/Diamond.cs
@@ -9,26 +9,27 @@ namespace DiamondApplication
     /// <summary>
     /// Klasa diament, umożliwiająca utworzenie próbek diamentu o określonej nazwie, id, domieszkowaniu oraz stosunku sp3/sp2
     /// </summary>
-    class Diamond
+   public class Diamond
     {
         int number;
         string name;
         string typeDoping;
         double ratio;
         double percentDoping;
-        
         /// <summary>
-        /// Konstruktor klasy diament
+        /// Konstruktor klasy diament, umożliwiający nadanie wstępnych wartości próbce
         /// </summary>
-        /// <param name="number">Id próbki w bazie danych</param>
-        /// <param name="name">Nazwa próbki </param>
+        /// <param name="number">Numer id próbki</param>
+        /// <param name="name">Nazwa próbki</param>
         /// <param name="ratio">Stosunek sp3/sp2</param>
-        /// <param name="percentDoping">Procent domieszkowania Borem</param>
-        Diamond(int number, string name, double ratio, double percentDoping)
+        /// <param name="typeDoping">Rodzaj domieszkowania</param>
+        /// <param name="percentDoping">Procent domieszkowania</param>
+        public Diamond(int number, string name, double ratio,string typeDoping, double percentDoping)
         {
             this.number = number;
             this.name = name;
             this.ratio = ratio;
+            this.typeDoping = typeDoping;
             this.percentDoping = percentDoping;
         }
     }
