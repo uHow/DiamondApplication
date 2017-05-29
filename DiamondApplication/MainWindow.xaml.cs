bb4a694d98d48fc571226204b9ddc87ec4964e1f
uHow <micrycew@student.pg.gda.pl>
2017-05-29 23:17:42 +0200

    Wprowadzono poprawki do wczytywania listy oraz dodawania rekordów w bazie danych.

--- DiamondApplication/MainWindow.xaml.cs
+++ DiamondApplication/MainWindow.xaml.cs
@@ -20,55 +20,24 @@ namespace DiamondApplication{
     public partial class MainWindow : Window{
 
         List<Diamond> diam;
-        Connector conn = new Connector();
-        List<String> names;
-        int j = 1;
+        Connector conn;
         public MainWindow(){
             InitializeComponent();
-            diam = new List<Diamond>();
-            names = new List<String>();
-            diamTable.ItemsSource = diam;
+            conn = new Connector();  
             conn.Connect("192.168.0.20", "username", "password", "project", "diamonds");
+            diam = conn.returnList();
+            conn.Disconnect();
+            diamTable.ItemsSource = diam;
         }
         private void AddButton_Click(object sender, RoutedEventArgs e){
             if (txtID.Text != null && txtName.Text != null){
+                conn.Connect("192.168.0.20", "username", "password", "project", "diamonds");
                 conn.Insert(int.Parse(txtID.Text), txtName.Text, double.Parse(txtRatio.Text), txttypeDoping.Text, double.Parse(txtpercentDoping.Text));
                 diam = conn.returnList();
-                //diam.Add(new Diamond(txtID.Text, txtName.Text, txtRatio.Text, txttypeDoping.Text, txtpercentDoping.Text));
+                diamTable.ItemsSource = diam;
+                conn.Disconnect();
             }
         }
-        private void RemoveButton_Click(object sender, RoutedEventArgs e){
-            conn.Delete(diam[listRemove.SelectedIndex].Number);
-            diam = conn.returnList();
-            //diam.Remove(diam[diamTable.SelectedIndex]);
-            //diamTable.SelectedItem = null;
-        }
-        private void UpdateButton_Click(object sender, RoutedEventArgs e) {
-            //conn.Update(diam[diamTable.SelectedIndex].Number, diam[diamTable.SelectedIndex].Name, diam[diamTable.SelectedIndex].Ratio, diam[diamTable.SelectedIndex].TypeDoping, diam[diamTable.SelectedIndex].PercentDoping);
-            conn.Update(int.Parse(updtxtID.Text), updtxtName.Text, double.Parse(updtxtRatio.Text), updtxttypeDoping.Text, double.Parse(updtxtpercentDoping.Text));
-            diam = conn.returnList();
-            //diam.Remove(diam[diamTable.SelectedIndex]);
-            //diamTable.SelectedItem = null;
-        }
-        public void getSamples(){
-            List<String> Samples = new List<String>();
-            int i=0;
-            while(i<diam.LongCount()){
-                Samples.Add(diam[i].Name);
-                i++;
-            }
-            if(Update.IsSelected == true)
-                listUpdate.ItemsSource = Samples;
-            if(Remove.IsSelected == true)
-                listRemove.ItemsSource = Samples;
-        }
-
-        private void listUpdate_SelectionChanged(object sender, SelectionChangedEventArgs e){
-            updtxtID.Text = diam[listUpdate.SelectedIndex].Number.ToString();
-            updtxtName.Text = diam[listUpdate.SelectedIndex].Name;
-            updtxtRatio.Text = diam[listUpdate.SelectedIndex].Ratio.ToString();
-            updtxtpercentDoping.Text = diam[listUpdate.SelectedIndex].PercentDoping.ToString();
-            updtxttypeDoping.Text = diam[listUpdate.SelectedIndex].TypeDoping;
-        }
+        
     }
 }
