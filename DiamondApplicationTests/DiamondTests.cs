31fc2642f47a238543c168216611f7212ca0529e
Michał Meironke <michal.meironke@gmail.com>
2017-06-15 22:49:45 +0200

    dodano testy dla klasy Diamond

--- DiamondApplicationTests/DiamondTests.cs
+++ DiamondApplicationTests/DiamondTests.cs
@@ -16,9 +16,35 @@ namespace DiamondApplication.Tests
         {
             Diamond sample = new Diamond(1, "nazwa", 5, "bor", 15);
             int numer = sample.Number;
-            Assert.AreEqual(numer, 1);
+            Assert.AreEqual(numer, sample.Number);
         }
 
-        
+        public void NameTest()
+        {
+            Diamond sample = new Diamond(1, "nazwa", 5, "bor", 15);
+            string nazwa = sample.Name;
+            Assert.AreEqual(nazwa, sample.Name);
+        }
+
+        public void RatioTest()
+        {
+            Diamond sample = new Diamond(1, "nazwa", 5, "bor", 15);
+            double stosunek = sample.Ratio;
+            Assert.AreEqual(stosunek, sample.Ratio);
+        }
+
+        public void TypeDopingTest()
+        {
+            Diamond sample = new Diamond(1, "nazwa", 5, "bor", 15);
+            string typ = sample.TypeDoping;
+            Assert.AreEqual(typ, sample.TypeDoping);
+        }
+
+        public void PercentDopingTest()
+        {
+            Diamond sample = new Diamond(1, "nazwa", 5, "bor", 15);
+            double procent = sample.PercentDoping;
+            Assert.AreEqual(procent, sample.PercentDoping);
+        }
     }
 }
